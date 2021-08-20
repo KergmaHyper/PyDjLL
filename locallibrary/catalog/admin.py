121cea1a7e510e6c29d06cf2615e73e70e53bd8d
kergma@gmail.com <kergma@gmail.com>
2021-08-20 16:08:55 +0300

    add models catalog and superuser admin (admin)

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre, Book, BookInstance, Author
+
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(BookInstance)
+
+
+
