d4a948462de60da018f3059d97784ef434116fe8
kergma@gmail.com <kergma@gmail.com>
2021-09-27 18:03:47 +0300

    JS

---
+++ learning/scripts/main.js
@@ -0,0 +1,14 @@
+var myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
+myHeading.textTitle = 'Hello!';
+
+function mult(num1, num2){
+ let m = num1 * num2;
+ return m;
+}
+
+//alert( mult(2,3) );
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
