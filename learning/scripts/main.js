f7d97c9822f3da5cab2aa816951f331d5e137146
kergma@gmail.com <kergma@gmail.com>
2021-09-27 23:22:47 +0300

    game

--- learning/scripts/main.js
+++ learning/scripts/main.js
@@ -9,6 +9,31 @@ function mult(num1, num2){
 
 //alert( mult(2,3) );
 
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+document.querySelector('h1').onclick = function() {
+    //alert('Ouch! Stop poking me!');
+    document.querySelector('h1').textContent =
+     prompt('Enter new header', document.querySelector('h1').textContent);
+
 }
+
+const para = document.querySelector('.ppp');
+para.addEventListener('click', updateName);
+function updateName() {
+ let name = prompt('Enter a new name', para.textContent);
+  para.textContent = 'Player 1: ' + name;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'You clicked the button! ' + buttons.length;
+    document.body.appendChild(para);
+  }
+
+  const buttons = document.querySelectorAll('button');
+
+  for(let i = 0; i < buttons.length ; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+  }
+});
